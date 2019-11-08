f62119417ec31f0c2d3e773533580434a6bc1dfc
sugspi <g1420542@is.ocha.ac.jp>
2019-11-08 09:34:11 +0900

    added oracle

--- src/main/scala/edin/ccg/MainCreateRule.scala
+++ src/main/scala/edin/ccg/MainCreateRule.scala
@@ -9,56 +9,60 @@ object MainCreateRule {
 
   def main(args: Array[String]): Unit = {
 
-    val file_train = "/Users/guru/MyResearch/Shift-Reduce/hw/train.trees"
-    val file_dev = "/Users/guru/MyResearch/Shift-Reduce/hw/dev.trees"
-    val file_test = "/Users/guru/MyResearch/Shift-Reduce/hw/test.trees"
+    val file_train = "/Users/guru/MyResearch/Shift-Reduce/org_data/train.trees"
+    val file_dev = "/Users/guru/MyResearch/Shift-Reduce/org_data/dev.trees"
+    val file_test = "/Users/guru/MyResearch/Shift-Reduce/org_data/test.trees"
 
-    val trees_train = DerivationsLoader.fromFile(file_train).toList
-    val trees_dev = DerivationsLoader.fromFile(file_dev).toList
+    //val trees_train = DerivationsLoader.fromFile(file_train).toList
+    //val trees_dev = DerivationsLoader.fromFile(file_dev).toList
     val trees_test = DerivationsLoader.fromFile(file_test).toList
-    val trees = trees_dev ++ trees_test ++ trees_train
+    //val trees = trees_dev ++ trees_test ++ trees_train
 
-    val table = createNonTerminalTable(trees)
-    val rules = findAllTheRules(trees, table)
+    //val table = createNonTerminalTable(trees)
+    //val rules = findAllTheRules(trees)//, table)
 
-    val pw_table = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/hw/ccg_rule_table")
-    val pw_ccg_trees_train = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/hw/ccg_train.tree")
-    val pw_ccg_trees_dev = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/hw/ccg_dev.tree")
-    val pw_ccg_trees_test = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/hw/ccg_test.tree")
+    //val pw_table = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/data/ccg_rule_table")
+    //val pw_ccg_trees_train = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/data/train.tree")
+    //val pw_ccg_trees_dev = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/data/dev.tree")
+    //val pw_ccg_trees_test = new PrintWriter("/Users/guru/MyResearch/Shift-Reduce/data/test.tree")
 
-    pw_table.println(rules.mkString("\n"))
+    //pw_table.println(rules.mkString("\n"))
     //println(rules.mkString("\n"))
 
-    for(tree <- trees_train){
-      pw_ccg_trees_train.println(tree2string(tree, table))
-    }
+    // for(tree <- trees_train){
+    //   pw_ccg_trees_train.println(tree2string(tree))
+    // }
 
-    for(tree <- trees_dev){
-      pw_ccg_trees_dev.println(tree2string(tree, table))
+    // for(tree <- trees_dev){
+    //   pw_ccg_trees_dev.println(tree2string(tree))
+    // }
 
-    }
+    // for(tree <- trees_test){
+    //   pw_ccg_trees_test.println(tree2string(tree))
+    // }
 
-    for(tree <- trees_test){
-      pw_ccg_trees_test.println(tree2string(tree, table))
-    }
+    // pw_table.close()
+    // pw_ccg_trees_train.close()
+    // pw_ccg_trees_dev.close()
+    // pw_ccg_trees_test.close()
 
-    pw_table.close()
-    pw_ccg_trees_train.close()
-    pw_ccg_trees_dev.close()
-    pw_ccg_trees_test.close()
+    output_oracle(trees_test(0))
 
+    //for(tree <- trees_toy){
+    //  output_oracle(tree)
+    //}
 
   }
 
-  def findAllTheRules(trees: List[TreeNode], table:Map[Category, String]) : Set[String] = {
+  def findAllTheRules(trees: List[TreeNode]) : Set[String] = {
     val allRules = scala.collection.mutable.Set[String]()
     for(tree <- trees){
       tree.allNodes.foreach{
         case node@UnaryNode(_, child) =>
-          allRules += s"${table(node.category)},${table(child.category)}"
+          allRules += s"${node.category},${child.category}"
           //allRules += s"${table(node.category)} -> ${table(child.category)}"
         case node@BinaryNode(_, left, right) =>
-          allRules += s"${table(node.category)},${table(left.category)},${table(right.category)}"
+          allRules += s"${node.category},${left.category},${right.category}"
           //allRules += s"${table(node.category)} -> ${table(left.category)} ${table(right.category)}"
         case _ =>
       }
@@ -82,24 +86,38 @@ object MainCreateRule {
     }.toMap
   }
 
-  def tree2string(node:TreeNode, table:Map[Category, String]) : String = node match {
-    case UnaryNode(_, child) => "("+table(node.category)+ " " +tree2string(child, table)+ ")"
-    case BinaryNode(_, left, right) => "(" +table(node.category)+ " " +tree2string(left, table) + " "+tree2string(right, table) + ")"
-    case TerminalNode(word, category) => s"(${table(category)} $word)"
+  def tree2string(node:TreeNode) : String = node match {
+    case UnaryNode(_, child) => "("+node.category+ " " +tree2string(child)+ ")"
+    case BinaryNode(_, left, right) => "(" +node.category+ " " +tree2string(left) + " "+tree2string(right) + ")"
+    case TerminalNode(word, category) => s"(${category} $word)"
   }
 
+
   def output_oracle(tree:TreeNode) {
-    for(node <- tree.allNodesPostorder){
-      node match {
-        case TerminalNode(_, tag) =>
-          println("SHIFT")
-          println(tag)
-        case BinaryNode(c, _, _) =>
-          println("REDUCE_B_"+c)
-        case UnaryNode(c, _) =>
-          println("REDUCE_U_"+c)
-      }
-    }
+
+    println("# "+tree2string(tree))
+    val category_tag = tree.category_leafs
+    for (c <- category_tag) { print(c + " ") }
+    println("")
+
+    val sentence = tree.words
+    for (s <- sentence) { print(s + " ") }
+    println("")
+    for (s <- sentence) { print(s.toLowerCase + " ") }
+    println("")
+    tree.allNodesPreorder2.foreach(println)
+
+    // for(node <- tree.allNodesPreorder2){
+    //   node match {
+    //     case TerminalNode(w, tag) =>
+    //       println("NT*" +tag+ "*") //+ w)
+    //       println("SHIFT")
+    //     case BinaryNode(c, _, _) =>
+    //       println("NT(" +c+ ")") //("REDUCE_B_"+c)
+    //     case UnaryNode(c, _) =>
+    //       println("NT(" +c+ ")") // ("REDUCE_U_"+c)
+    //   }
+    // }
   }
 
 }
